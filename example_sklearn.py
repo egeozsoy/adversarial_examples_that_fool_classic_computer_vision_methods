f672038f5bb05a4dda0ff699d198237ec3f78e9c
egeozsoy <egeozsoy97@gmail.com>
2019-05-02 08:13:09 +0200

    - cleanup code - implement loaders - implement save and reload - implement new datasets

--- example_sklearn.py
+++ example_sklearn.py
@@ -2,49 +2,18 @@ from foolbox.models import Model
 import foolbox
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
-from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import fetch_openml
+
 from sklearn.model_selection import train_test_split
-from load_cifar10_data import get_data
+from loader import load_data
 import numpy as np
 from utils import plot_result
 import cv2
+import os
 
-#TODO visualize sift function
-#TODO mnist and fashion mnist
-# TODO easier cifar 10 with only 2 classes
 
-def one_hot_to_class(one_hot):
-    return np.argmax(one_hot,axis=1)
-
-def show_cifar_10_image(image):
-    cv2.imshow('image',cv2.cvtColor(np.uint8(image), cv2.COLOR_RGB2BGR))
-    cv2.waitKey(0)
-
-# just return flattened image
-def dummy_feature_extractor(images):
-    return images.reshape(-1, 32 * 32 * 3)
-
-# output sift descriptors
-def extract_sift_features(image):
-    grayscale = cv2.cvtColor(np.uint8(image), cv2.COLOR_RGB2GRAY)
-    kp, desc = sift.detectAndCompute(grayscale,None)
-    return kp, desc
-
-def bovw_extractor(images):
-    bovws = None
-    for image in images:
-        grayscale = cv2.cvtColor(np.uint8(image), cv2.COLOR_RGB2GRAY)
-        siftkp = sift.detect(grayscale)
-        bov = bow_extract.compute(grayscale,siftkp)
-        if bov is None:
-            # set bov to zero if no sift features were found
-            bov = np.zeros((1,vocab_size))
-        if bovws is None:
-            bovws = bov
-        else:
-            bovws = np.concatenate([bovws,bov])
-
-    return bovws
+# TODO easier cifar 10 with only 4 classes
 
 class FoolboxSklearnWrapper(Model):
 
@@ -62,49 +31,167 @@ class FoolboxSklearnWrapper(Model):
         return self.predictor.predict(features)
 
 
-if __name__ == '__main__':
-    #Hyperparams
-    vocab_size = 2000
-    data_size = 60000
-    feature_extractor = dummy_feature_extractor
+def show_cifar_10_image(image):
+    cv2.imshow('image', cv2.cvtColor(np.uint8(image), cv2.COLOR_RGB2BGR))
+    cv2.waitKey(0)
 
+
+def show_imagenette_image(image):
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+
+
+def show_mnist_image(image):
+    cv2.imshow('image', np.uint8(image))
+    cv2.waitKey(0)
+
+
+def visualize_sift_points(image):
+    kp, desc = extract_sift_features(image)
+    gray = cv2.cvtColor(np.uint8(image), cv2.COLOR_RGB2GRAY)
+    gray = np.uint8(image)
+    img = cv2.drawKeypoints(gray, kp, None)
+    cv2.imshow('sift_keypoints.jpg', img)
+    cv2.waitKey(0)
+
+
+# just return flattened image
+def dummy_feature_extractor(images):
+    return images.reshape(images.shape[0], -1) / 255
+
+
+# output sift descriptors
+def extract_sift_features(image):
+    grayscale = cv2.cvtColor(np.uint8(image), cv2.COLOR_RGB2GRAY)
+    grayscale = np.uint8(image)
+    kp, desc = sift.detectAndCompute(grayscale, None)
+    return kp, desc
+
+
+def bovw_extractor(images):
+    bovws = None
+    for image in images:
+        grayscale = cv2.cvtColor(np.uint8(image), cv2.COLOR_RGB2GRAY)
+        grayscale = np.uint8(image)
+        siftkp = sift.detect(grayscale)
+        bov = bow_extract.compute(grayscale, siftkp)
+        if bov is None:
+            # set bov to zero if no sift features were found
+            bov = np.zeros((1, vocab_size))
+        if bovws is None:
+            bovws = bov
+        else:
+            bovws = np.concatenate([bovws, bov])
+
+    return bovws
+
+
+# delete classes that are not in keep_classes, simplify cifar-10
+def filter_classes(X, y, keep_classes):
+    logic_result = y == keep_classes[0]
+    for keep_class in keep_classes[1:]:
+        logic_result = np.logical_or(logic_result, y == keep_class)
+
+    # X = X[logic_result, :, :, :]
+    X = X[logic_result, :, :]
+    y = y[logic_result]
+
+    return X, y
+
+
+if __name__ == '__main__':
+    # Hyperparams
+    vocab_size = 1000
+    data_size = 2000
+    feature_extractor = bovw_extractor
+    visualize_sift = False
+    image_size = 32 * 5
+    use_classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    n_features = 0  # means sift chooses
+    dataset_name = 'imagenette'
+
+    iter_name = 'iter_dtn_{}_vs{}_ds{}_is{}_cc{}_nf{}_fe_{}'.format(dataset_name, vocab_size, data_size, image_size, len(use_classes), n_features,
+                                                                    feature_extractor.__name__)
+
+    print('Running iteration {}'.format(iter_name))
 
     # initilize features to use
-    sift = cv2.xfeatures2d.SIFT_create(contrastThreshold = 0.02,edgeThreshold = 25)
-    matcher = cv2.FlannBasedMatcher(dict(algorithm=1, trees=5), {})
-    ## 1. setup BOW
-    bow_train = cv2.BOWKMeansTrainer(vocab_size)  # toy world, you want more.
-    bow_extract = cv2.BOWImgDescriptorExtractor(sift, matcher)
+    sift = cv2.xfeatures2d.SIFT_create(nfeatures=n_features)
 
-    data = np.float32(get_data()[b'data'])[0:data_size]
-    labels = np.float32(get_data()[b'labels'])[0:data_size]
+    print('Loading Data')
+    X, y = load_data(image_size, dataset_name=dataset_name)
 
-    X_train, X_test, y_train, y_test = train_test_split(data,labels,test_size=0.2,shuffle=True)
+    # X, y = filter_classes(X,y,keep_classes=use_classes)
 
-    # 2. Fill bow
-    images_with_problems = 0
-    for idx,train_image in enumerate(X_train):
+    # we might want to resize images
+    if image_size != X.shape[1]:
+        print('Resizing images')
+        X_resized = []
+        for img in X:
+            X_resized.append(cv2.resize(img, (image_size, image_size)))
 
-        kp,desc = extract_sift_features(train_image)
-        if desc is None:
-            images_with_problems += 1
+        X = np.array(X_resized)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+
+    X_train = X_train[:data_size]
+    X_test = X_test[:data_size]
+    y_train = y_train[:data_size]
+    y_test = y_test[:data_size]
+
+    print('Dataset size {}'.format(X_train.shape[0]))
+
+    if visualize_sift:
+        print('Visualising images with sift points')
+        for a in range(0, 100):
+            print(y_train[a:a + 1])
+            show_imagenette_image(X_train[a])
+            visualize_sift_points(X_train[a])
+
+    if feature_extractor == bovw_extractor:
+
+        matcher = cv2.FlannBasedMatcher(dict(algorithm=1, trees=5), {})
+        ## 1. setup BOW
+        bow_extract = cv2.BOWImgDescriptorExtractor(sift, matcher)
+
+        if not os.path.exists('voc_{}.npy'.format(iter_name)):
+            bow_train = cv2.BOWKMeansTrainer(vocab_size)  # toy world, you want more.
+            # 2. Fill bow
+            print('Calculating Vocabulary for training images')
+            images_with_problems = 0
+            for idx, train_image in enumerate(X_train):
+
+                kp, desc = extract_sift_features(train_image)
+                if desc is None:
+                    images_with_problems += 1
+                else:
+                    bow_train.add(desc)
+
+            print('No sift features were found for {} images'.format(images_with_problems))
+            print('Generating vocabulary by clustering')
+
+            voc = bow_train.cluster()
+            np.save('voc_{}.npy'.format(iter_name), voc)
+
         else:
-            bow_train.add(desc)
+            print('Loaded Vocabulary')
+            voc = np.load('voc_{}.npy'.format(iter_name))
 
-    print('No sift features were found for {} images'.format(images_with_problems))
-    voc = bow_train.cluster()
-    bow_extract.setVocabulary(voc)
+        bow_extract.setVocabulary(voc)
 
-    model = RandomForestClassifier(n_estimators=100)
-    model = LinearSVC(max_iter=20)
-
-    y_train = one_hot_to_class(y_train)
-    y_test = one_hot_to_class(y_test)
+    # model = RandomForestClassifier(n_estimators=100)
+    model = LinearSVC()
+    # model = LogisticRegression()
 
     print('Starting model training')
-    model.fit(feature_extractor(X_train/255), y_train)
-    print('Training set performance {}'.format(model.score(feature_extractor(X_train/255), y_train)))
-    print('Testing set performance {}'.format(model.score(feature_extractor(X_test/255), y_test)))
+    model.fit(feature_extractor(X_train), y_train)
+    print('Training set performance {}'.format(model.score(feature_extractor(X_train), y_train)))
+    print('Testing set performance {}'.format(model.score(feature_extractor(X_test), y_test)))
+
+    print('Sample predictions from training {}'.format(model.predict(feature_extractor(X_train[:10]))))
+    print('Ground truth for        training {}'.format(y_train[:10]))
+    print('Sample predictions from testing {}'.format(model.predict(feature_extractor(X_test[:10]))))
+    print('Ground truth for        testing {}'.format(y_test[:10]))
 
     # adversarial = None
     # # stop if unsuccessful after #timeout trials
---
+++ loader.py
@@ -0,0 +1,193 @@
+import os
+import cv2
+
+
+import numpy as np
+from sklearn.datasets import fetch_openml
+
+datasets_root = 'datasets'
+
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        data = pickle.load(fo, encoding='bytes')
+    return data
+
+
+def load_cifar_10_data(get_test=False):
+    file1 = os.path.join(datasets_root, "cifar-10-batches-py/data_batch_1")
+    file2 = os.path.join(datasets_root, "cifar-10-batches-py/data_batch_2")
+    file3 = os.path.join(datasets_root, "cifar-10-batches-py/data_batch_3")
+    file4 = os.path.join(datasets_root, "cifar-10-batches-py/data_batch_4")
+    file5 = os.path.join(datasets_root, "cifar-10-batches-py/data_batch_5")
+    test_file = os.path.join(datasets_root, 'cifar-10-batches-py/test_batch')
+
+    if not get_test:
+        data1 = unpickle(file1)
+        data2 = unpickle(file2)
+        data3 = unpickle(file3)
+        data4 = unpickle(file4)
+        data5 = unpickle(file5)
+        data = {b'data': np.concatenate((data1[b'data'], data2[b'data'], data3[b'data'], data4[b'data'], data5[b'data'])),
+                b'labels': np.concatenate((data1[b'labels'], data2[b'labels'], data3[b'labels'], data4[b'labels'], data5[b'labels']))}
+        data[b'data'] = np.transpose(data[b'data'].reshape(-1, 3, 32, 32), (0, 2, 3, 1))
+        return np.float32(data[b'data']), np.float32(data[b'labels'])
+
+    else:
+        data = unpickle(test_file)
+        data[b'data'] = np.transpose(data[b'data'].reshape(-1, 3, 32, 32), (0, 2, 3, 1))
+        return np.float32(data[b'data']), np.float32(data[b'labels'])
+
+
+def load_mnist_data():
+    mnist_folder = os.path.join(datasets_root, 'mnist')
+    if not os.path.exists(mnist_folder):
+        # data not found
+        os.mkdir(mnist_folder)
+        X, y = fetch_openml('mnist_784', return_X_y=True)
+        X = X.reshape(-1, 28, 28)
+        y = np.uint8(y)
+        np.save(os.path.join(mnist_folder, 'X.npy'), X)
+        np.save(os.path.join(mnist_folder, 'y.npy'), y)
+
+    else:
+        # data found
+        X = np.load(os.path.join(mnist_folder, 'X.npy'))
+        y = np.load(os.path.join(mnist_folder, 'y.npy'))
+
+    return X, y
+
+
+def load_fmnist_data():
+    fmnist_folder = os.path.join(datasets_root, 'fmnist')
+    if not os.path.exists(fmnist_folder):
+        # data not found
+        os.mkdir(fmnist_folder)
+        X, y = fetch_openml('Fashion-MNIST', return_X_y=True)
+        X = X.reshape(-1, 28, 28)
+        y = np.uint8(y)
+        np.save(os.path.join(fmnist_folder, 'X.npy'), X)
+        np.save(os.path.join(fmnist_folder, 'y.npy'), y)
+
+    else:
+        # data found
+        X = np.load(os.path.join(fmnist_folder, 'X.npy'))
+        y = np.load(os.path.join(fmnist_folder, 'y.npy'))
+
+    return X, y
+
+def load_imagenette_data(image_size,get_test=False):
+    imagenette_folder = os.path.join(datasets_root,'imagenette-160')
+    folder_to_load = 'train' if get_test == False else 'val'
+    X_name = 'X_{}.npy'.format(folder_to_load)
+    y_name = 'y_{}.npy'.format(folder_to_load)
+
+    if os.path.exists(os.path.join(imagenette_folder,X_name)):
+        print('Loading preprocessed data')
+        X = np.load(os.path.join(imagenette_folder,X_name))
+        y = np.load(os.path.join(imagenette_folder, y_name))
+
+    else:
+        folder_to_load = 'train' if get_test == False else 'val'
+
+        folder_to_load_path = os.path.join(imagenette_folder,folder_to_load)
+
+        X = []
+        y = []
+
+        class_index = 0
+
+        for subfolder in sorted(os.listdir(folder_to_load_path)):
+            sub_folder_path = os.path.join(folder_to_load_path,subfolder)
+            if not os.path.isdir(sub_folder_path):
+                continue
+            for img in os.listdir(sub_folder_path):
+                #make sure actually picture
+                img_path = os.path.join(sub_folder_path,img)
+                if '.JPEG' in img_path:
+                    current_image = cv2.resize(cv2.imread(img_path),(image_size,image_size))
+                    X.append(current_image)
+                    y.append(class_index)
+
+            class_index += 1
+
+        X = np.array(X)
+        y = np.array(y)
+        np.save(os.path.join(imagenette_folder, X_name),X)
+        np.save(os.path.join(imagenette_folder, y_name),y)
+
+
+    return X,y
+
+
+
+#pascal challange
+# this should already resize while returning I think
+# def load_voc2007_data(image_size,get_test=False):
+#     voc2007 = os.path.join(datasets_root,'VOC2007')
+#     images_path = os.path.join(voc2007,'JPEGImages')
+#     labels_path = os.path.join(voc2007,'ImageSets/Main')
+#     train_file = os.path.join(labels_path,'train.txt')
+#     test_file = os.path.join(labels_path,'val.txt')
+#
+#     load_file = train_file if get_test == False else test_file
+#     lookfor_str = '_train.txt' if get_test == False else '_val.txt'
+#
+#     X = []
+#
+#     # we first have to load the image_names without labels
+#     with open(train_file) as f:
+#         lines = f.readlines()
+#         for name in lines:
+#             if name != '':
+#                 X.append(name.strip())
+#
+#     X = np.array(X)
+#     y = np.zeros((X.shape[0],)) - 1 #initilize labels with -1
+#
+#     class_index = 0
+#
+#     for label_file in sorted(os.listdir(labels_path)):
+#         if lookfor_str in label_file:
+#             label_file_path = os.path.join(labels_path,label_file)
+#             with open(label_file_path) as f:
+#                 lines = f.readlines()
+#                 labels = []
+#                 for line in lines:
+#                     line = line.strip()
+#                     label = line.split(' ')[-1]
+#                     labels.append(int(label))
+#
+#                 labels = np.array(labels)
+#                 # Mask and set those indices to class_index
+#                 mask = labels == 1
+#                 y[mask] = class_index
+#
+#
+#             class_index += 1
+#
+#     print(X)
+#     print(y)
+#
+#     print('Resizing images')
+#     X_resized = []
+#     for img_path in X:
+#         img = cv2.imread(os.path.join(images_path,img_path))
+#         X_resized.append(cv2.resize(img, (image_size, image_size // 1,333)))
+#
+#     X = np.array(X_resized)
+#
+#     return X,y
+
+def load_data(image_size,get_test=False,dataset_name='imagenette'):
+    if dataset_name == 'mnist':
+        return load_mnist_data()
+    elif dataset_name =='fmnist':
+        return load_fmnist_data()
+    elif dataset_name == 'imagenette':
+        return load_imagenette_data(image_size,get_test=get_test)
+    elif dataset_name == 'cifar-10':
+        return load_cifar_10_data(image_size,get_test=get_test)
+
+    raise Exception('Not a valid dataset')
